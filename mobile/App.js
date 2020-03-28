9329f42c055966508829fe44f104e94cb843059e
ViniMonteiro1990 <vinimonteirolg50@gmail.com>
2020-03-28 10:54:32 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';//essa importacao serve para mostrar o R$ na frente
+
+import React from 'react';
+
+
+import Routes from './src/routes'
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
